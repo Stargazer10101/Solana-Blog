fcb0a31bfafe0690df71b5fc8682bd4205fe399d
Nitish Itankar <nitishitankar9102@gmail.com>
2024-05-04 16:54:59 +0530

    First commit- Adding files

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -2,13 +2,26 @@ import { useMemo } from "react"
 import { BlogProvider } from "src/context/Blog"
 import { Router } from "src/router"
 import "./App.css"
-
+import { ConnectionProvider, WalletProvider } from "@solana/wallet-adapter-react"
+import { PhantomWalletAdapter } from "@solana/wallet-adapter-wallets"
+import "./App.css"
 
 export const App = () => {
-
+const endpoint= "https://tame-light-dream.solana-devnet.quiknode.pro/37c7908dfddddc8873e0a4c0ff374ff3899ab515/"
+const wallets = useMemo(
+  ()=> [
+    new PhantomWalletAdapter(),
+  ],
+  []
+)
   return (
-    <BlogProvider>
-      <Router />
-    </BlogProvider>
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets} autoConnect>
+        <BlogProvider>
+          <Router />
+        </BlogProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+    
   )
 }
